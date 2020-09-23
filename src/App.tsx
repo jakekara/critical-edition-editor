1a462a8ae5e880604df1031ca09857aa7e4e116d
Jake Kara <jake@jakekara.com>
2020-09-23 14:30:53 -0400

    Install react-editor-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import styles from "./App.module.css";
+
+import EditorJs from "react-editor-js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles["App"]}>
+      <EditorJs autofocus holder="editor-js-holder-1">
+        <div className={styles["editor-js-holder"]} id="editor-js-holder-1" />
+      </EditorJs>
     </div>
   );
 }
