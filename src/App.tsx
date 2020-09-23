732e421b24ee5710e44694df0f4699e40b8e78df
Jake Kara <jake@jakekara.com>
2020-09-23 16:59:09 -0400

    Implement basic editor with footnotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import EditorJs from "react-editor-js";
+import { Footnote, FootnoteMaker } from "editorjs-footnotes";
+import { API, OutputData } from "@editorjs/editorjs";
 
 import styles from "./App.module.css";
-
-import EditorJs from "react-editor-js";
+import * as api from "./api";
 
 function App() {
   return (
     <div className={styles["App"]}>
-      <EditorJs autofocus holder="editor-js-holder-1">
+      <EditorJs
+        data={api.loadTemporaryData()}
+        autofocus
+        holder="editor-js-holder-1"
+        tools={{
+          FootnoteMaker,
+          footnoteParagraph: {
+            class: Footnote,
+            inlineToolbar: ["link", "bold", "italic"], // don't allow footnotes to add footnotes
+          },
+        }}
+        onChange={(_: API, data?: OutputData) => {
+          if (!data) {
+            return;
+          }
+
+          api.saveTemporaryData(data);
+        }}
+      >
         <div className={styles["editor-js-holder"]} id="editor-js-holder-1" />
       </EditorJs>
     </div>
