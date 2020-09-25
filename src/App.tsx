86b799c3ab3d273a42b70edf601fbe02aabf966d
Jake Kara <jake@jakekara.com>
2020-09-25 13:58:36 -0400

    Added basic import and export

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import EditorJs from "react-editor-js";
 import { Footnote, FootnoteMaker } from "editorjs-footnotes";
 import { API, OutputData } from "@editorjs/editorjs";
@@ -7,10 +7,65 @@ import styles from "./App.module.css";
 import * as api from "./api";
 
 function App() {
+  const [data, setData] = useState<any>(api.loadTemporaryData());
+  console.log("Rendering with data", data);
+
   return (
     <div className={styles["App"]}>
+      <button
+        id="save"
+        onClick={async () => {
+          const data = api.loadTemporaryData();
+          if (!data) {
+            return;
+          }
+
+          const fileName = "critical-edition";
+          const json = JSON.stringify(data);
+          const blob = new Blob([json], { type: "application/json" });
+          const href = await URL.createObjectURL(blob);
+          const link = document.createElement("a");
+          link.href = href;
+          link.download = fileName + ".json";
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }}
+      >
+        Export draft
+      </button>
+      <input
+        type="file"
+        id="file-selector"
+        accept=".json"
+        onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+          const target = evt.target as HTMLInputElement;
+          const files = target.files;
+          if (!files) {
+            return;
+          }
+          if (files) console.log(`${files[0]}`);
+
+          const reader = new FileReader();
+          reader.addEventListener("load", (evt) => {
+            console.log("Loaded data", evt.target?.result);
+            let data = evt.target?.result;
+            if (typeof data !== "string") {
+              return;
+            }
+            if (!data) {
+              return;
+            }
+            data = JSON.parse(data);
+            setData(data);
+            console.log("Set data to ", data);
+          });
+          reader.readAsText(files[0]);
+        }}
+      ></input>
       <EditorJs
-        data={api.loadTemporaryData()}
+        enableReInitialize
+        data={data}
         autofocus
         holder="editor-js-holder-1"
         tools={{
@@ -26,6 +81,7 @@ function App() {
           }
 
           api.saveTemporaryData(data);
+          console.log("Saving data", data);
         }}
       >
         <div className={styles["editor-js-holder"]} id="editor-js-holder-1" />
